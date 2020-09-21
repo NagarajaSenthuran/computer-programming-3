106b883de3c09662cb6e62e89ba17db317128f74
ymsilhan <ymsilhan@gmail.com>
2020-09-21 10:11:31 +0530

    changes in java files

---
+++ forStash/Document.java
@@ -0,0 +1,30 @@
+public class Document{
+
+public String Name;
+public String Extension;
+public String content;
+public int Id;
+
+public void setName(){
+
+}
+
+public void setExtension(){
+
+}
+
+public void setContent(){
+
+}
+
+public String getName(){
+
+}
+public String getName(){
+
+}
+public String getName(){
+
+}
+
+}
